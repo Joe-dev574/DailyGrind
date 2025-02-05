f14e97b4c3313f285c9f4d4e093af9be3d1d4b89
JDeWeese <jdeweese574@icloud.com>
2025-02-05 08:45:54 -0600

    Initial Commit

---
+++ DailyGrindTests/DailyGrindTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyGrindTests.swift
+//  DailyGrindTests
+//
+//  Created by Joseph DeWeese on 2/5/25.
+//
+
+import Testing
+@testable import DailyGrind
+
+struct DailyGrindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
